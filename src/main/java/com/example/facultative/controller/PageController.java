7228f9ab9283bcc18e25781754fd05b83049d998
Nazar <nazzzario23@gmail.com>
2020-10-02 21:58:40 +0300

    fix login for authenticated user

--- src/main/java/com/example/facultative/controller/PageController.java
+++ src/main/java/com/example/facultative/controller/PageController.java
@@ -1,5 +1,8 @@
 package com.example.facultative.controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,14 @@ public class PageController {
 
     @RequestMapping("/login")
     public String login() {
-        return "login";
+        return isAuthenticated() ? "redirect:/" : "login";
+    }
+
+    private boolean isAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || AnonymousAuthenticationToken.class.isAssignableFrom(authentication.getClass())) {
+            return false;
+        }
+        return authentication.isAuthenticated();
     }
 }
